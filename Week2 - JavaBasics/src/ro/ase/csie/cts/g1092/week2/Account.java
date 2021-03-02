c34f6349d0ad27b44fe6805114eb7823e31a49b0
Rares999 <rares.iurea99@yahoo.com>
2021-03-02 12:05:43 +0200

    Week2Final

--- Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
@@ -1,10 +1,10 @@
 package ro.ase.csie.cts.g1092.week2;
 
+
+
 public abstract class Account {
 	public abstract double getBalance();
 	public abstract void deposit(double amount);
-	public abstract void transfer(Account destination, double amount);
-	public abstract void withdraw(double amount);
-		
-	}
-
+	public abstract void transfer(Account destination, double amount) throws InsufficientFundsException, IllegalTransferException;
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
+}
---
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1092.week2;
+
+public class InsufficientFundsException extends Exception {
+
+	public InsufficientFundsException(String string) {
+		
+	}
+
+}

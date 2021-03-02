44fda3cd539136d664ff6fe6cf54538a49eef6e0
Rares999 <rares.iurea99@yahoo.com>
2021-03-02 10:42:10 +0200

    Week2 corrected version

--- Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/exceptions/InsuficientDFoundsExceptoin.java
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/exceptions/InsuficientDFoundsExceptoin.java
@@ -1,7 +1,7 @@
 package ro.ase.csie.cts.g1092.week2.exceptions;
 
 public class InsuficientDFoundsExceptoin extends Exception {
-	public InsuficientDFoundsExceptoin(String messsage) {
+	public InsuficientDFoundsExceptoin(String message) {
 		super(message);
 		
 	}
